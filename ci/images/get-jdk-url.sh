11f4b27b056c9272affdaf3003c9d80ba18511d1
dieguesroveda <dieguesroveda@gmail.com>
2024-04-27 15:40:00 -0300

    Initial commit

---
+++ ci/images/get-jdk-url.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+case "$1" in
+	java17)
+		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.10%2B13/bellsoft-jdk17.0.10+13-linux-amd64.tar.gz"
+	;;
+  *)
+		echo $"Unknown java version"
+		exit 1
+esac
